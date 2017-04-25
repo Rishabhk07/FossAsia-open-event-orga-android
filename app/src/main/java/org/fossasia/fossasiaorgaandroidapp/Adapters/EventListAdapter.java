3735572f35f0b60e834896cf4547e17a890d8533
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-04-26 03:59:41 +0530

    user event list done
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/org/fossasia/fossasiaorgaandroidapp/Adapters/EventListAdapter.java
+++ app/src/main/java/org/fossasia/fossasiaorgaandroidapp/Adapters/EventListAdapter.java
@@ -1,45 +1,70 @@
 package org.fossasia.fossasiaorgaandroidapp.Adapters;
 
+import android.app.Activity;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.fossasia.fossasiaorgaandroidapp.R;
+import org.fossasia.fossasiaorgaandroidapp.model.UserEvents;
+
+import java.util.ArrayList;
 
 /**
  * Created by rishabhkhanna on 26/04/17.
  */
 
-public class EventListAdapter {
+public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.EventRecyclerViewHolder>{
 
-    public void newInstance(){
+    ArrayList<UserEvents> eventsArrayList = new ArrayList<>();
+    Activity activity;
+
+    public EventListAdapter(ArrayList<UserEvents> eventsArrayList ,Activity context) {
+        this.eventsArrayList = eventsArrayList;
+        this.activity = context;
+    }
+
+    @Override
+    public EventRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater li = activity.getLayoutInflater();
+        View itemView = li.inflate(R.layout.event_layout,null);
+
+        EventRecyclerViewHolder eventRecyclerViewHolder  = new EventRecyclerViewHolder(itemView);
+        return eventRecyclerViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(EventRecyclerViewHolder holder, int position) {
+
+        UserEvents thisEvent = eventsArrayList.get(position);
+        holder.eventTitle.setText(thisEvent.getName());
+        Picasso.with(activity).load(thisEvent.getThumbnail()).into(holder.eventImage);
 
     }
 
-    public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.EventRecyclerViewHolder>{
+    @Override
+    public int getItemCount() {
+        return eventsArrayList.size();
+    }
 
 
-        @Override
-        public EventRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
-        }
+    //view holder class
+    public  class EventRecyclerViewHolder extends RecyclerView.ViewHolder{
+        TextView eventTitle;
+        ImageView eventImage;
 
-        @Override
-        public void onBindViewHolder(EventRecyclerViewHolder holder, int position) {
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return 0;
-        }
-
-        public  class EventRecyclerViewHolder extends RecyclerView.ViewHolder{
-
-            public EventRecyclerViewHolder(View itemView) {
-                super(itemView);
-            }
+        public EventRecyclerViewHolder(View itemView) {
+            super(itemView);
+            eventTitle = (TextView) itemView.findViewById(R.id.tvEventTitle);
+            eventImage = (ImageView) itemView.findViewById(R.id.ivEventProfile);
         }
 
     }
-
 
 }
--- app/src/main/java/org/fossasia/fossasiaorgaandroidapp/Views/EventsActivity.java
+++ app/src/main/java/org/fossasia/fossasiaorgaandroidapp/Views/EventsActivity.java
@@ -2,33 +2,46 @@ package org.fossasia.fossasiaorgaandroidapp.Views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.android.volley.VolleyError;
 import com.google.gson.Gson;
 
+import org.fossasia.fossasiaorgaandroidapp.Adapters.EventListAdapter;
 import org.fossasia.fossasiaorgaandroidapp.Api.ApiCall;
 import org.fossasia.fossasiaorgaandroidapp.Api.LoginCall;
 import org.fossasia.fossasiaorgaandroidapp.R;
 import org.fossasia.fossasiaorgaandroidapp.Utils.Constants;
 import org.fossasia.fossasiaorgaandroidapp.model.UserEvents;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class EventsActivity extends AppCompatActivity {
 
     public static final String TAG = "EventsActivity";
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+        recyclerView = (RecyclerView) findViewById(R.id.rvEventList);
+
         LoginCall.VolleyCallBack volleyCallBack = new LoginCall.VolleyCallBack() {
             @Override
             public void onSuccess(String response) {
                 Gson gson = new Gson();
                 UserEvents[] userEvents = gson.fromJson(response , UserEvents[].class);
+                userEvents = Arrays.copyOfRange(userEvents,1,userEvents.length);
                 for (UserEvents event : userEvents){
                     Log.d(TAG, "onSuccess: " + event.getName());
                 }
+
+                ArrayList<UserEvents> userEventsArrayList = new ArrayList<>(Arrays.asList(userEvents));
+                inflateEvents(userEventsArrayList);
 
             }
 
@@ -39,4 +52,15 @@ public class EventsActivity extends AppCompatActivity {
         };
         ApiCall.callApi(this , Constants.userEvents, volleyCallBack);
     }
+
+    public void inflateEvents(ArrayList<UserEvents> eventsArrayList){
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+
+        EventListAdapter eventListAdapter = new EventListAdapter(eventsArrayList , this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(eventListAdapter);
+        eventListAdapter.notifyDataSetChanged();
+
+    }
+
 }
