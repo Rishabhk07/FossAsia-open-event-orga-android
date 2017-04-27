5b0471c3408e94ee46d641e56df33aba370fbc45
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-04-27 06:59:11 +0530

    Attendee checkin layout
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/org/fossasia/fossasiaorgaandroidapp/Views/AttendeeListActivity.java
+++ app/src/main/java/org/fossasia/fossasiaorgaandroidapp/Views/AttendeeListActivity.java
@@ -7,6 +7,7 @@ import org.fossasia.fossasiaorgaandroidapp.R;
 
 public class AttendeeListActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
